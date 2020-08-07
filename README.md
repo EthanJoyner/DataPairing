5ec1acc7792c17b8e9aa69ecc0cc08eb53a571f2
EthanJoyner <el2joyner@gmail.com>
2020-08-07 16:33:48 -0400

    Python code and name intp

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DataPairing
 Paring user data into an algorithm 
+
+### The Process reqires 2 inputs from CSV files. 
+- Name
+- Personal code
+
+#### Personal Code
+The personal code is an indicator code that can determine the perfecnce work locatation of a volenteer as well as the Perfered working positions.#####Ex: 0000pdf
+- 1st asks NW? 
+- 2nd asks NE?
+- 3rd asks SW?
+- 4th asks SE?
+- 5th asks packaging?
+- 6th asks driving? 
+- 7th asks farming?
+
--- pcd.py
+++ pcd.py
@@ -1,20 +1,47 @@
 import csv
+
+#Name list maker
+nameList = []
+with open('names.csv') as names_csv:
+    for name in names_csv:
+        print(name)
+        nameList.append(name.replace("\n",""))
+print("nameList:")
+print(nameList)
+
+# Code list Maker
 codeList =[]
 with open('pcdCodes.csv') as codes_csv:
     currentCode= ""
     for line in codes_csv:
         idx=0
         for chara in line:
-            print(chara)
             if idx % 2 == 0:
                 currentCode = currentCode + chara
             else:
                 currentCode = currentCode
             idx += 1 
-        print(currentCode)
         codeList.append(currentCode)
         currentCode = ""
+print("codeList:")
 print(codeList)
+
+
+# Binder
+nameCodeList=[]
+smallsec=[]
+if len(codeList) == len(nameList):
+    thisIdx = 0
+    for x in codeList:
+        smallsec.append(nameList[thisIdx])
+        smallsec.append(codeList[thisIdx])
+        nameCodeList.append(smallsec)
+        smallsec=[]
+        thisIdx +=1
+    print("Full Result:")
+    print(nameCodeList)
+else:
+    print("The csv files are out of sync")
 
 
 
@@ -35,26 +62,41 @@ print(codeList)
 #0000 _ _ _ is the key
 #Q1, q2, q3, q4, delivery, Packaging, Farming
 
-sample = "1111pdf"
-print(sample[0])
 
-# q1 check
-if sample[0] == "1":
-    print("NW covered")
-else:
-    print("void")
-# q2 check
-if sample[1] == "1":
-    print("NE covered")
-else:
-    print("void")
-# q3 check
-if sample[2] == "1":
-    print("SW covered")
-else:
-    print("void")
-# q4 check
-if sample[3] == "1":
-    print("SE covered")
-else:
-    print("void")
+
+def codeCheck (sample):
+    # q1 check
+    if sample[0] == "1":
+        print("NW covered")
+    else:
+        print("void")
+    # q2 check
+    if sample[1] == "1":
+        print("NE covered")
+    else:
+        print("void")
+    # q3 check
+    if sample[2] == "1":
+        print("SW covered")
+    else:
+        print("void")
+    # q4 check
+    if sample[3] == "1":
+        print("SE covered")
+    else:
+        print("void")
+    # packaging check
+    if sample[4] == "p":
+        print("Packaging Confimed")
+    else: 
+        print("void")    
+    # driving check
+    if sample[5] == "d":
+        print("Driving Confirmed")
+    else: 
+        print("void")  
+    # Farming check
+    if sample[6] =="f":
+        print("Farming Confirmed")
+    else: 
+        print("void")
